be9c79b2d6114cdb3352b0c415cc6e75fb2790f2
shift02 <shift02ss@gmail.com>
2018-07-09 22:09:55 +0900

    修正：弓兵

--- src/main/java/littleservantmod/profession/ProfessionArcher.java
+++ src/main/java/littleservantmod/profession/ProfessionArcher.java
@@ -39,7 +39,7 @@ public class ProfessionArcher extends ProfessionLSMBase implements IRangedAttack
 	public void attackEntityWithRangedAttack(EntityLivingBase target, float distanceFactor) {
 		EntityArrow entityarrow = this.getArrow(distanceFactor);
 		double d0 = target.posX - this.servant.getEntityInstance().posX;
-		double d1 = target.getEntityBoundingBox().minY + target.height / 3.0F - entityarrow.posY;
+		double d1 = target.getEntityBoundingBox().minY + target.height / 3.0F - entityarrow.posY - 1;
 		double d2 = target.posZ - this.servant.getEntityInstance().posZ;
 		double d3 = MathHelper.sqrt(d0 * d0 + d2 * d2);
 		entityarrow.shoot(d0, d1 + d3 * 0.20000000298023224D, d2, 1.6F, 14 - this.servant.getEntityInstance().world.getDifficulty().getDifficultyId() * 4);
