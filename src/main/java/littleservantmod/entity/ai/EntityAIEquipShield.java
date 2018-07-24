0bdd6bccf5009897dc89ee7201b505575ad90803
Kina <CrafterKina@users.noreply.github.com>
2018-07-25 01:20:42 +0900

    変更：装備変更をアイテムの位置変更ではなく手ごとの参照位置変更によって実現

--- src/main/java/littleservantmod/entity/ai/EntityAIEquipShield.java
+++ src/main/java/littleservantmod/entity/ai/EntityAIEquipShield.java
@@ -4,6 +4,7 @@ import littleservantmod.entity.EntityLittleServant;
 import net.minecraft.init.Items;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumHand;
 
 import java.util.function.Predicate;
 
@@ -17,7 +18,7 @@ public class EntityAIEquipShield extends EntityAIEquip {
 	private static final Predicate<ItemStack> IS_SHIELD = SHIELD::isItemEqualIgnoreDurability;
 
 	public EntityAIEquipShield(EntityLittleServant servant) {
-		super(servant, EntityEquipmentSlot.OFFHAND, IS_SHIELD);
+		super(servant, EnumHand.OFF_HAND, IS_SHIELD);
 	}
 
 }
--- src/main/java/littleservantmod/entity/ai/EntityAIEquipTool.java
+++ src/main/java/littleservantmod/entity/ai/EntityAIEquipTool.java
@@ -3,12 +3,13 @@ package littleservantmod.entity.ai;
 import littleservantmod.entity.EntityLittleServant;
 import littleservantmod.profession.ProfessionToolManager;
 import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.ResourceLocation;
 
 public class EntityAIEquipTool extends EntityAIEquip {
 
 	public EntityAIEquipTool(EntityLittleServant servant, ResourceLocation type) {
-		super(servant, EntityEquipmentSlot.MAINHAND, stack -> ProfessionToolManager.getInstance().isTool(type, stack));
+		super(servant, 0, EnumHand.MAIN_HAND, stack -> ProfessionToolManager.getInstance().isTool(type, stack));
 	}
 
 }
