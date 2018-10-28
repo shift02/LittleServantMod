0cb8afffea6ff7123bb2902532205836f4d9b1cf
shift02 <shift02ss@gmail.com>
2018-10-28 18:05:12 +0900

    更新：バージョン

--- src/main/java/littleservantmod/LittleServantMod.java
+++ src/main/java/littleservantmod/LittleServantMod.java
@@ -43,7 +43,7 @@ public class LittleServantMod {
     /** MOD名称 */
     public static final String MOD_NAME = "LittleServantMod";
     /** MODのバージョン */
-    public static final String MOD_VERSION = "0.0.6";
+    public static final String MOD_VERSION = "0.0.7";
 
     private static Logger logger;
 
