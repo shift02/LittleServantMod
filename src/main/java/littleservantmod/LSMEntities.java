6a81006b7653df37b2d3a3f478e16446816d007d
shift02 <shift02ss@gmail.com>
2019-01-13 10:34:48 +0900

    追加：Entityの登録を別クラスに移動

---
+++ src/main/java/littleservantmod/LSMEntities.java
@@ -0,0 +1,40 @@
+package littleservantmod;
+
+import littleservantmod.client.renderer.entity.RenderEntityLittleServant;
+import littleservantmod.entity.EntityLittleServant;
+import net.minecraft.client.renderer.entity.Render;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.client.registry.IRenderFactory;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.EntityRegistry;
+
+public class LSMEntities {
+
+    private final FMLPreInitializationEvent event;
+
+    public LSMEntities(FMLPreInitializationEvent event) {
+        this.event = event;
+    }
+
+    public void registerEntities() {
+
+        int trackingRange = 80;
+        int updateFrequency = 3;
+        boolean sendVelocityUpdates = true;
+        EntityRegistry.registerModEntity(new ResourceLocation(LittleServantMod.MOD_ID, "little_servant"), EntityLittleServant.class,
+                "little_servant", 0, this, trackingRange, updateFrequency, sendVelocityUpdates, 0xffffff, 0xFF0000);
+
+        if (!event.getSide().isServer()) {
+            RenderingRegistry.registerEntityRenderingHandler(EntityLittleServant.class, new IRenderFactory() {
+                @Override
+                public Render createRenderFor(RenderManager manager) {
+                    return new RenderEntityLittleServant(manager);
+                }
+            });
+        }
+
+    }
+
+}
--- src/main/java/littleservantmod/LittleServantMod.java
+++ src/main/java/littleservantmod/LittleServantMod.java
@@ -3,19 +3,12 @@ package littleservantmod;
 import org.apache.logging.log4j.Logger;
 
 import littleservantmod.api.LittleServantModAPI;
-import littleservantmod.client.renderer.entity.RenderEntityLittleServant;
-import littleservantmod.entity.EntityLittleServant;
 import littleservantmod.packet.LSMPacketHandler;
 import littleservantmod.profession.DefaultProfessionEventHandler;
 import littleservantmod.profession.ProfessionManager;
 import littleservantmod.profession.ServantToolManager;
-import net.minecraft.client.renderer.entity.Render;
-import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.launchwrapper.Launch;
-import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.client.registry.IRenderFactory;
-import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
@@ -23,7 +16,6 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
-import net.minecraftforge.fml.common.registry.EntityRegistry;
 
 @Mod(modid = LittleServantMod.MOD_ID, name = LittleServantMod.MOD_NAME, version = LittleServantMod.MOD_VERSION, useMetadata = true)
 public class LittleServantMod {
@@ -42,6 +34,8 @@ public class LittleServantMod {
     @Instance(MOD_ID)
     public static LittleServantMod instance;
 
+    public LSMEntities lSMEntities;
+
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         logger = event.getModLog();
@@ -49,20 +43,9 @@ public class LittleServantMod {
 
         LittleServantModAPI.register = new LSMRegister();
 
-        int trackingRange = 80;
-        int updateFrequency = 3;
-        boolean sendVelocityUpdates = true;
-        EntityRegistry.registerModEntity(new ResourceLocation(MOD_ID, "little_servant"), EntityLittleServant.class,
-                "little_servant", 0, this, trackingRange, updateFrequency, sendVelocityUpdates, 0xffffff, 0xFF0000);
-
-        if (!event.getSide().isServer()) {
-            RenderingRegistry.registerEntityRenderingHandler(EntityLittleServant.class, new IRenderFactory() {
-                @Override
-                public Render createRenderFor(RenderManager manager) {
-                    return new RenderEntityLittleServant(manager);
-                }
-            });
-        }
+        //Entityの登録
+        this.lSMEntities = new LSMEntities(event);
+        this.lSMEntities.registerEntities();
 
         LSMProxy.getProxy().fmlPreInit();
         NetworkRegistry.INSTANCE.registerGuiHandler(this, LSMProxy.getProxy());
